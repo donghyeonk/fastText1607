7c91e93e58bb9691b1b4d0a92d04ff422929c902
donghyeon_kim <donghyeon@korea.ac.kr>
2018-07-13 16:25:18 +0900

    Add validation

--- dataset.py
+++ dataset.py
@@ -9,9 +9,12 @@ class AGData(object):
         self.config = config
         self.data_path = config.data_path
         self.n_train_examples = config.n_train_examples
+        self.n_valid_examples = config.n_valid_examples
         self.n_test_examples = config.n_test_examples
         self.num_classes = config.num_classes
         self.top_categories = self.get_top_categories(topn=self.num_classes)
+        self.max_len = config.max_len
+
         # TODO hashing trick
         self.ngram2idx = dict()
         self.idx2ngram = dict()
@@ -20,12 +23,12 @@ class AGData(object):
         self.ngram2idx['UNK'] = 1
         self.idx2ngram[1] = 'UNK'
 
-        self.max_len = 100
-
-        self.train_data, self.test_data = self.load()
+        self.train_data, self.valid_data, self.test_data = self.load()
+        self.count_labels()
 
     def load(self):
         train_data = list()
+        valid_data = list()
         test_data = list()
 
         cat_counts = [0] * self.num_classes
@@ -56,75 +59,88 @@ class AGData(object):
                 if category not in self.top_categories:
                     continue
 
-                cat_counts[self.top_categories.index(category)] += 1
-                if cat_counts[self.top_categories.index(category)] > \
-                        (self.n_train_examples + self.n_test_examples):
+                category_idx = self.top_categories.index(category)
+                cat_counts[category_idx] += 1
+                if cat_counts[category_idx] > \
+                        (self.n_train_examples + self.n_valid_examples +
+                         self.n_test_examples):
                     continue
 
+                is_valid = \
+                    self.n_train_examples < cat_counts[category_idx] <= \
+                    self.n_train_examples + self.n_valid_examples
                 is_test = \
-                    cat_counts[self.top_categories.index(category)] > \
-                    self.n_train_examples
+                    cat_counts[category_idx] > \
+                    self.n_train_examples + self.n_valid_examples
 
                 title = cols[2]
                 description = cols[5]
                 title_desc = title + ' ' + description
 
-                # b_o_w = ['<s>'] + title_desc.split(' ') + ['</s>']
-                b_o_w = \
-                    ['<s>'] \
-                    + [token.text for token in nlp(title_desc)] \
-                    + ['</s>']
+                # n_grams_test = list(zip(*[title_desc.split(" ")[i:] for i in range(2)]))
+
+                # create bag-of-ngrams
+                b_o_w = [token.text for token in nlp(title_desc)]
                 ngram = get_ngram(b_o_w, n=self.config.n_gram)
                 b_o_ngrams = b_o_w + ngram
+
+                bon_len = len(b_o_ngrams)
+
+                # stats
+                if max_len_real < bon_len:
+                    max_len_real = bon_len
+                x_lens.append(bon_len)
                 for ng in b_o_ngrams:
                     ngram_set.add(ng)
 
-                    # update dict.
+                # limit max len
+                if self.max_len < bon_len:
+                    # self.max_len = len(b_o_ngrams)
+                    b_o_ngrams = b_o_ngrams[:self.max_len]
+
+                # update dict.
+                for ng in b_o_ngrams:
                     idx = self.ngram2idx.get(ng)
                     if idx is None and not is_test:
                         idx = len(self.ngram2idx)
                         self.ngram2idx[ng] = idx
                         self.idx2ngram[idx] = ng
 
-                bon_len = len(b_o_ngrams)
-
-                if self.max_len < bon_len:
-                    # self.max_len = len(b_o_ngrams)
-                    b_o_ngrams = b_o_ngrams[:self.max_len]
-
-                if max_len_real < bon_len:
-                    max_len_real = bon_len
-
-                x_lens.append(bon_len)
-
+                # assign ngram idxs
                 x = [self.ngram2idx[ng] if ng in self.ngram2idx
                      else self.ngram2idx['UNK']
                      for ng in b_o_ngrams]
+
+                # padding
                 while len(x) < self.max_len:
                     x.append(self.ngram2idx['PAD'])
                 assert len(x) == self.max_len
 
-                y = self.top_categories.index(category)
+                y = category_idx
 
-                if not is_test:
-                    train_data.append(x + [y])
-                else:
+                if is_test:
                     test_data.append(x + [y])
+                elif is_valid:
+                    valid_data.append(x + [y])
+                else:
+                    train_data.append(x + [y])
 
-                if (len(train_data) + len(test_data)) % 10000 == 0:
+                if (len(train_data) + len(valid_data) + len(test_data)) \
+                        % 10000 == 0:
                     print(datetime.now(), line_cnt,
-                          len(train_data), len(test_data))
+                          len(train_data), len(valid_data), len(test_data))
 
         print('\nlines', line_cnt)
         print('errs', errs)
         print('# of unique ngrams', len(ngram_set))
+        print('dictionary size', len(self.ngram2idx))
         print('max_len (setting)', self.max_len)
         print('max_len (real)', max_len_real)
         print('avg_len {:.1f}'.format(sum(x_lens) / len(x_lens)))
         print('max_len coverage {:.3f}'.format(
               sum([1 for xl in x_lens if xl <= self.max_len]) / len(x_lens)))
 
-        return train_data, test_data
+        return train_data, valid_data, test_data
 
     def get_top_categories(self, topn=4):
         category_dict = dict()
@@ -133,8 +149,8 @@ class AGData(object):
             lines = f.read().split('\\N')
             for line in lines:
                 # line = line.replace('\t\t', '\t')
-                if '\t\t' in line:
-                    pass
+                # if '\t\t' in line:
+                #     pass
                 cols = line.split('\t')
                 if 6 > len(cols):
                     continue
@@ -154,9 +170,33 @@ class AGData(object):
             top_categories.append(cat)
         return top_categories
 
+    def count_labels(self):
+        def count(data):
+            count_dict = dict()
+            for d in data:
+                cnt = count_dict.get(d[-1])
+                if cnt is None:
+                    count_dict[d[-1]] = 1
+                else:
+                    count_dict[d[-1]] = cnt + 1
+            print(count_dict)
+
+        count(self.train_data)
+        count(self.valid_data)
+        count(self.test_data)
+
     def get_dataloaders(self, batch_size=32, shuffle=True, num_workers=8):
         train_loader = torch.utils.data.DataLoader(
             AGDataset(self.train_data),
+            shuffle=shuffle,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            collate_fn=batchify,
+            pin_memory=True
+        )
+
+        valid_loader = torch.utils.data.DataLoader(
+            AGDataset(self.valid_data),
             shuffle=shuffle,
             batch_size=batch_size,
             num_workers=num_workers,
@@ -171,10 +211,11 @@ class AGData(object):
             collate_fn=batchify,
             pin_memory=True
         )
-        return train_loader, test_loader
+        return train_loader, valid_loader, test_loader
 
 
 def get_ngram(words, n=2):
+    # TODO add ngrams up to n
     return [' '.join(words[i: i+n]) for i in range(len(words)-(n-1))]
 
 
@@ -199,15 +240,20 @@ if __name__ == '__main__':
     import argparse
     import os
     import pickle
+    import pprint
     parser = argparse.ArgumentParser()
     # http://www.di.unipi.it/~gulli/newsspace200.xml.bz
     parser.add_argument('--data_path', type=str, default='./data/newsSpace')
     parser.add_argument('--pickle_path', type=str, default='./data/ag.pkl')
     parser.add_argument('--num_classes', type=int, default=4)
     parser.add_argument('--n_gram', type=int, default=2)
-    parser.add_argument('--n_train_examples', type=int, default=30000)
+    parser.add_argument('--n_train_examples', type=int, default=27000)
+    parser.add_argument('--n_valid_examples', type=int, default=3000)
     parser.add_argument('--n_test_examples', type=int, default=1900)
+    parser.add_argument('--max_len', type=int, default=200)
     args = parser.parse_args()
+
+    pprint.PrettyPrinter().pprint(args.__dict__)
 
     if os.path.exists(args.pickle_path):
         with open(args.pickle_path, 'rb') as f_pkl:
@@ -217,8 +263,8 @@ if __name__ == '__main__':
         with open(args.pickle_path, 'wb') as f_pkl:
             pickle.dump(agdata, f_pkl)
 
-    tr_loader, _ = agdata.get_dataloaders(batch_size=24, num_workers=4)
+    tr_loader, _, _ = agdata.get_dataloaders(batch_size=24, num_workers=8)
     print(len(tr_loader.dataset))
     for batch_idx, batch in enumerate(tr_loader):
-        if batch_idx % 1000 == 0:
-            print(batch_idx)
+        if (batch_idx + 1) % 1000 == 0:
+            print(batch_idx + 1)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 import torch
 from torch import nn, optim
 import torch.nn.functional as F
@@ -8,9 +9,9 @@ from dataset import AGData
 # https://github.com/bentrevett/pytorch-sentiment-analysis/blob/master/3%20-%20Faster%20Sentiment%20Analysis.ipynb
 
 
-class fastText(nn.Module):
+class FastText(nn.Module):
     def __init__(self, config):
-        super(fastText, self).__init__()
+        super(FastText, self).__init__()
         self.config = config
 
         self.bon_embed = nn.Embedding(config.vocab_size, config.embedding_dim,
@@ -18,11 +19,7 @@ class fastText(nn.Module):
         self.hidden = nn.Linear(config.embedding_dim, config.hidden_size)
         self.fc = nn.Linear(config.hidden_size, config.num_classes)
 
-        # init.
-        nn.init.xavier_uniform_(self.hidden.weight, gain=1)
-        nn.init.uniform_(self.hidden.bias)
-        nn.init.xavier_uniform_(self.fc.weight, gain=1)
-        nn.init.uniform_(self.fc.bias)
+        self.init_linears()
 
         self.optimizer = optim.SGD(self.parameters(), lr=config.lr)
         self.criterion = nn.NLLLoss()
@@ -47,9 +44,15 @@ class fastText(nn.Module):
 
         return F.log_softmax(self.fc(hdn), dim=1)
 
+    def init_linears(self):
+        nn.init.xavier_uniform_(self.hidden.weight, gain=1)
+        nn.init.uniform_(self.hidden.bias)
+        nn.init.xavier_uniform_(self.fc.weight, gain=1)
+        nn.init.uniform_(self.fc.bias)
+
     def lr_decay(self, epoch):
         next_lr = self.config.lr * (1. - epoch / self.config.epochs)
-        print('learning rate is to be {:.3f}'.format(next_lr))
+        print('Next learning rate: {:.3f}'.format(next_lr))
         for param_group in self.optimizer.param_groups:
             param_group['lr'] = next_lr
 
@@ -73,17 +76,19 @@ def train(device, loader, model, epoch, config):
         if (batch_idx + 1) % config.log_interval == 0 \
                 or batch_idx == len(loader) - 1:
             _progress = \
-                '\r{} Train Epoch {}, [{}/{} ({:.1f}%)]\tLoss: {:.6f}' \
+                '\r{} Train Epoch {}, [{}/{} ({:.1f}%)],\tBatch Loss: {:.6f}' \
                 .format(datetime.now(), epoch,
                         example_count, len(loader.dataset),
                         100. * example_count / len(loader.dataset),
                         batch_loss / len(outputs))
             print(_progress)
     train_loss /= len(loader.dataset)
+    print('{} Train Epoch {}, Avg. Loss: {:.6f}'.format(datetime.now(), epoch,
+                                                        train_loss))
     return train_loss
 
 
-def test(device, loader, model, epoch):
+def evaluate(device, loader, model, epoch, mode):
     model.eval()
     eval_loss = 0.
     correct = 0
@@ -98,31 +103,55 @@ def test(device, loader, model, epoch):
             correct += pred.eq(target.view_as(pred)).sum().item()
     eval_loss /= len(loader.dataset)
     acc = correct / len(loader.dataset)
-    print('{} Test Epoch {}, Loss: {:.6f}, Accuracy: {}/{} ({:.2f}%)'.format(
-        datetime.now(), epoch, eval_loss, correct, len(loader.dataset),
-        100. * acc))
+    print('{} {} Epoch {}, Avg. Loss: {:.6f}, '
+          'Accuracy: {}/{} ({:.2f}%)'.format(datetime.now(), mode, epoch,
+                                             eval_loss,
+                                             correct, len(loader.dataset),
+                                             100. * acc))
     return eval_loss, acc
+
+
+def save_model(model, args, model_save_path):
+    # save a model and args
+    model_dict = dict()
+    model_dict['state_dict'] = model.state_dict()
+    model_dict['m_config'] = args
+    model_dict['m_optimizer'] = model.optimizer.state_dict()
+    # now_t = datetime.now().strftime("%Y%m%d_%H%M")[2:]
+    if not os.path.exists(args.checkpoint_dir):
+        os.makedirs(args.checkpoint_dir)
+    torch.save(model_dict, model_save_path)
+    print('Saved', model_save_path)
+
+
+def load_model(model, load_path):
+    print('\t-> load checkpoint %s' % load_path)
+    checkpoint = torch.load(load_path)
+    model.load_state_dict(checkpoint['state_dict'])
+    # model.optimizer.load_state_dict(checkpoint['m_optimizer'])
 
 
 def main():
     import argparse
     import pickle
+    import pprint
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', type=str, default="fastText_")
+    parser.add_argument('--name', type=str, default='fastText1607')
+    parser.add_argument('--checkpoint_dir', type=str, default='./ckpt/')
     parser.add_argument('--seed', type=int, default=2018)
     parser.add_argument('--data_path', type=str, default='./data/ag.pkl')
     parser.add_argument('--hidden_size', type=int, default=10)
     parser.add_argument('--num_classes', type=int, default=4)
 
     parser.add_argument('--n_grams', type=int, default=2)
-    parser.add_argument('--embedding_dim', type=int, default=100)  #
+    parser.add_argument('--embedding_dim', type=int, default=50)  #
     parser.add_argument('--n_train_examples', type=int, default=30000)
     parser.add_argument('--n_test_examples', type=int, default=1900)
 
-    parser.add_argument('--lr', type=float, default=5e-2)  #
+    parser.add_argument('--lr', type=float, default=5e-1)  #
     parser.add_argument('--wd', type=float, default=0)  #
     parser.add_argument('--batch_size', type=int, default=256)  #
-    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--epochs', type=int, default=20)
     parser.add_argument('--log_interval', type=int, default=100)
     args = parser.parse_args()
 
@@ -131,23 +160,42 @@ def main():
 
     with open(args.data_path, 'rb') as f:
         ag_dataset = pickle.load(f)
-
     args_dict = vars(args)
     args_dict['vocab_size'] = len(ag_dataset.ngram2idx)
-    print('vocab_size', args_dict['vocab_size'])
-
-    ft = fastText(args).to(device)
-    train_loader, test_loader = \
+    pprint.PrettyPrinter().pprint(args.__dict__)
+    train_loader, valid_loader, test_loader = \
         ag_dataset.get_dataloaders(batch_size=args.batch_size)
 
+    ft = FastText(args).to(device)
+
     best_acc = 0.
+    best_loss = float('inf')
     for epoch in range(1, args.epochs + 1):
         train(device, train_loader, ft, epoch, args)
-        _, acc = test(device, test_loader, ft, epoch)
-        if acc > best_acc:
-            best_acc = acc
-        print('\tBest Acc. {:.2f}%'.format(100 * best_acc))
-        ft.lr_decay(epoch)
+        valid_loss, valid_acc = \
+            evaluate(device, valid_loader, ft, epoch, 'Valid')
+        if valid_loss < best_loss:
+            best_loss = valid_loss
+            best_acc = valid_acc
+            save_model(ft, args, os.path.join(args.checkpoint_dir,
+                                              '{}.pth'.format(args.name)))
+        else:
+            # TODO early stopping
+            pass
+
+        print('\tLowest Loss {:.6f}, Acc. {:.2f}%'.format(best_loss,
+                                                          100 * best_acc))
+
+        # optional
+        evaluate(device, test_loader, ft, epoch, 'Test')
+
+        if epoch < args.epochs:
+            ft.lr_decay(epoch)
+
+    # load the best
+    load_model(ft, os.path.join(args.checkpoint_dir,
+                                '{}.pth'.format(args.name)))
+    evaluate(device, test_loader, ft, args.epochs, 'Test')
 
 
 if __name__ == '__main__':
